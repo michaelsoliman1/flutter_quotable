1b4d840766bc3f38457a656d20473243548233ce
MichaelSoliman <michaelsoliman981@gmail.com>
2022-11-01 13:06:15 +0200

    added some clarification to QuoteModel

--- lib/app/quotes/data/models/quote_model.dart
+++ lib/app/quotes/data/models/quote_model.dart
@@ -3,6 +3,13 @@ import 'package:quotes/app/quotes/domain/domain.dart';
 
 part 'quote_model.g.dart';
 
+// Why`IMPLEMENT` Quote?
+// since the name of one of our fields is not the same as the api,
+// we want to stick to our domain names, we have two options here,
+// the first one is implementing the [Quote] interface like below,
+// or second, overriding only the field we want to rename, like in the this issue,
+// https://github.com/google/json_serializable.dart/issues/1073
+// both have some drawbacks
 @JsonSerializable()
 class QuoteModel implements Quote {
   QuoteModel({
